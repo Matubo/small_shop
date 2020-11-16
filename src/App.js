79ea4adc46eb3586eaee54ad240426d6b669c5a1
Matthew <Matbei14@yandex.ru>
2020-11-16 14:11:08 +0300

    App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { useState, useReducer } from "react";
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import MenuSpace from "./features/elements/reactElements/menu";
 import StoreSpace from "./features/elements/reactElements/storeSpace";
@@ -8,8 +6,7 @@ import Footer from "./features/elements/reactElements/footer";
 
 function App() {
   return (
-    //<DeleteFromApp/>
-    <div className="app">
+    <div className="app_body">
       <MenuSpace />
       <StoreSpace />
       <Footer />
